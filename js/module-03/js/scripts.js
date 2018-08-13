47328dba4e53327a403fb7bd577befc7adfa622f
Ivan <nerty777@gmail.com>
2018-08-13 15:03:04 +0300

    fix homework-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -2,6 +2,29 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 let allLogins = logins;
 
+const isLoginValid = login => {
+  return login.length >= 4 && login.length <= 16;
+};
+
+const isLoginUnique = (login, allLogins = logins) => {
+  return allLogins.includes(login);
+};
+
+const addLogin = function(login, allLogins = logins) {
+  if (isLoginValid(login) === false) {
+    return alert(
+      `Ошибка! Логин должен быть от 4 до 16 символов. Вы ввели ${
+        login.length
+      } указав логин ${login}`
+    );
+  }
+  if (isLoginUnique(login, allLogins) === true) {
+    return alert(`Логин ${login} уже используется!`);
+  }
+  logins.push(login);
+  return alert(`Логин ${login} успешно добавлен!`);
+};
+
 do {
   let login = prompt("Введите логин");
 
@@ -15,34 +38,13 @@ do {
     continue;
   }
 
-  const isLoginValid = login => {
-    return login.length >= 4 && login.length <= 16;
-  };
-
-  const isLoginUnique = (login, allLogins = logins) => {
-    return allLogins.includes(login);
-  };
-
-  const addLogin = function(login, allLogins = logins) {
-    if (isLoginValid(login) === false) {
-      return alert(
-        `Ошибка! Логин должен быть от 4 до 16 символов. Вы ввели ${
-          login.length
-        } указав логин ${login}`
-      );
-    }
-    if (isLoginUnique(login, allLogins) === true) {
-      return alert(`Логин ${login} уже используется!`);
-    }
-    logins.push(login);
-    return alert(`Логин ${login} успешно добавлен!`);
-  };
-
   addLogin(login, allLogins);
+
+
+} while (true);
 
   // Вызовы функции для проверки
   addLogin("Ajax"); // 'Логин успешно добавлен!'
   addLogin("robotGoogles"); // 'Такой логин уже используется!'
   addLogin("Zod"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
   addLogin("jqueryisextremelyfast"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-} while (true);
