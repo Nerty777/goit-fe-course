d101dd54627ca104466c221f15fce50294a6d92c
Ivan <nerty777@gmail.com>
2018-08-14 23:28:40 +0300

    fix homework-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -1,35 +1,26 @@
 "use strict";
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const isLoginValid = login => {
-  return login.length >= 4 && login.length <= 16;
-};
+const isLoginValid = login => login.length >= 4 && login.length <= 16;
 
-const isLoginUnique = (login, logins) => {
-  return logins.includes(login);
-};
+const isLoginUnique = (login, logins) => logins.includes(login);
 
 const addLogin = function(login, logins) {
   if (!isLoginValid(login)) {
-    return alert("Ошибка! Логин должен быть от 4 до 16 символов.");
+    alert(`Ошибка! Логин ${login} должен быть от 4 до 16 символов.`);
+    return;
   }
   if (isLoginUnique(login, logins)) {
-    return alert(`Логин ${login} уже используется!`);
+    alert(`Логин ${login} уже используется!`);
+    return;
   }
   logins.push(login);
   alert(`Логин ${login} успешно добавлен!`);
   return true;
 };
 
-do {
-  let login = prompt("Введите логин");
-
-  if (login === null) {
-    alert("Отменено пользователем");
-    break;
-  }
-
-  if (addLogin(login, logins)) {
-    break;
-  }
-} while (true);
+// Вызовы функции для проверки
+addLogin("Ajax", logins); // 'Логин успешно добавлен!'
+addLogin("robotGoogles", logins); // 'Такой логин уже используется!'
+addLogin("Zod", logins); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin("jqueryisextremelyfast", logins); // 'Ошибка! Логин должен быть от 4 до 16 символов'
