9aea10752042b6d77589f19bf6d6b88615cf462d
Ivan <nerty777@gmail.com>
2018-08-14 18:35:05 +0300

    decrease homework-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -1,44 +1,22 @@
 "use strict";
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
-let allLogins = logins;
-let success = 0;
-const symbol = "символ";
-const symbol2 = "символа";
-const symbol3 = "символов";
 
 const isLoginValid = login => {
   return login.length >= 4 && login.length <= 16;
 };
 
-// функция склонение существительных с числительными для вывода правильного окончания "символ" или "символа" или "символов"
-const pluralForm = function(login, symbol, symbol2, symbol3) {
-  let length = login.length;
-  length = length % 100;
-  const $n1 = length % 10;
-  if (length > 10 && length < 20) return symbol3;
-  if ($n1 > 1 && $n1 < 5) return symbol2;
-  if ($n1 == 1) return symbol;
-  return symbol3;
+const isLoginUnique = (login, logins) => {
+  return logins.includes(login);
 };
 
-const isLoginUnique = (login, allLogins = logins) => {
-  return allLogins.includes(login);
-};
-
-const addLogin = function(login, allLogins = logins) {
+const addLogin = function(login, logins) {
   if (!isLoginValid(login)) {
-    const theend = pluralForm(login, symbol, symbol2, symbol3);
-    return alert(
-      `Ошибка! Логин должен быть от 4 до 16 символов. Вы ввели ${
-        login.length
-      } ${theend} указав логин ${login}`
-    );
+    return alert("Ошибка! Логин должен быть от 4 до 16 символов.");
   }
-  if (isLoginUnique(login, allLogins)) {
+  if (isLoginUnique(login, logins)) {
     return alert(`Логин ${login} уже используется!`);
   }
   logins.push(login);
-  success = 1;
   return alert(`Логин ${login} успешно добавлен!`);
 };
 
@@ -50,16 +28,5 @@ do {
     break;
   }
 
-  if (+login === 0 && login !== "0") {
-    alert("Не был введен логин, попробуйте еще раз");
-    continue;
-  }
-
-  addLogin(login, allLogins);
-} while (success === 0);
-
-// Вызовы функции для проверки
-addLogin("Ajax"); // 'Логин успешно добавлен!'
-addLogin("robotGoogles"); // 'Такой логин уже используется!'
-addLogin("Zod"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-addLogin("jqueryisextremelyfast"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+  addLogin(login, logins);
+} while (true);
---
+++ js/module-03/js/truescripts.js
@@ -0,0 +1,65 @@
+"use strict";
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+let allLogins = logins;
+let success = 0;
+const symbol = "символ";
+const symbol2 = "символа";
+const symbol3 = "символов";
+
+const isLoginValid = login => {
+  return login.length >= 4 && login.length <= 16;
+};
+
+// функция склонение существительных с числительными для вывода правильного окончания "символ" или "символа" или "символов"
+let pluralForm = function(login, symbol, symbol2, symbol3) {
+  let length = login.length;
+  length = length % 100;
+  const $n1 = length % 10;
+  if (length > 10 && length < 20) return symbol3;
+  if ($n1 > 1 && $n1 < 5) return symbol2;
+  if ($n1 == 1) return symbol;
+  return symbol3;
+};
+
+const isLoginUnique = (login, allLogins = logins) => {
+  return allLogins.includes(login);
+};
+
+const addLogin = function(login, allLogins = logins) {
+  if (!isLoginValid(login)) {
+    let theend = pluralForm(login, symbol, symbol2, symbol3);
+    return alert(
+      `Ошибка! Логин должен быть от 4 до 16 символов. Вы ввели ${
+        login.length
+      } ${theend} указав логин ${login}`
+    );
+  }
+  if (isLoginUnique(login, allLogins)) {
+    return alert(`Логин ${login} уже используется!`);
+  }
+  logins.push(login);
+  success = 1;
+  return alert(`Логин ${login} успешно добавлен!`);
+};
+
+do {
+  let login = prompt("Введите логин");
+
+  if (login === null) {
+    alert("Отменено пользователем");
+    break;
+  }
+
+  if (+login === 0 && login !== "0") {
+    alert("Не был введен логин, попробуйте еще раз");
+    continue;
+  }
+
+  addLogin(login, allLogins);
+} while (success === 0);
+
+// Вызовы функции для проверки
+addLogin("Ajax"); // 'Логин успешно добавлен!'
+addLogin("robotGoogles"); // 'Такой логин уже используется!'
+addLogin("Zod"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin("jqueryisextremelyfast"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
