853398acee3c5c709f2b68bba542431d182d09a3
Ivan <nerty777@gmail.com>
2018-08-14 11:32:37 +0300

    fix homework-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -2,9 +2,23 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 let allLogins = logins;
 let success = 0;
+const symbol = "символ";
+const symbol2 = "символа";
+const symbol3 = "символов";
 
 const isLoginValid = login => {
   return login.length >= 4 && login.length <= 16;
+};
+
+// функция склонение существительных с числительными для вывода правильного окончания "символ" или "символа" или "символов"
+let pluralForm = function(login, symbol, symbol2, symbol3) {
+  let length = login.length;
+  length = length % 100;
+  const $n1 = length % 10;
+  if (length > 10 && length < 20) return symbol3;
+  if ($n1 > 1 && $n1 < 5) return symbol2;
+  if ($n1 == 1) return symbol;
+  return symbol3;
 };
 
 const isLoginUnique = (login, allLogins = logins) => {
@@ -13,10 +27,11 @@ const isLoginUnique = (login, allLogins = logins) => {
 
 const addLogin = function(login, allLogins = logins) {
   if (!isLoginValid(login)) {
+    const theend = pluralForm(login, symbol, symbol2, symbol3);
     return alert(
       `Ошибка! Логин должен быть от 4 до 16 символов. Вы ввели ${
         login.length
-      } указав логин ${login}`
+      } ${theend} указав логин ${login}`
     );
   }
   if (isLoginUnique(login, allLogins)) {
